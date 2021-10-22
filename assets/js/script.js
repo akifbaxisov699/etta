1210044d51c873fe35caf35c58c344ed9175d182
akifbaxisov699 <92740575+akifbaxisov699@users.noreply.github.com>
2021-10-22 11:58:35 -0700

    Project-add

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@
 
 
 $(document).ready(function () {
-    $(".nav-item").hover(function(){
-       $(this).addClass("show");
-       $(this).find(".dropdown-menu").addClass("show");
-    },function(){
+    $(".nav-item").hover(function () {
+        $(this).addClass("show");
+        $(this).find(".dropdown-menu").addClass("show");
+    }, function () {
         $(this).removeClass("show");
         $(this).find(".dropdown-menu").removeClass("show");
     })
@@ -53,3 +53,72 @@ $('.product-slider').owlCarousel({
     }
 })
 
+$(document).ready(function () {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
+
+function magnify(imgID, zoom) {
+    var img, glass, w, h, bw;
+    img = document.getElementById(imgID);
+    glass = document.createElement("DIV");
+    glass.setAttribute("class", "img-magnifier-glass");
+    img.parentElement.insertBefore(glass, img);
+    glass.style.backgroundImage = "url('" + img.src + "')";
+    glass.style.backgroundRepeat = "no-repeat";
+    glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+    bw = 3;
+    w = glass.offsetWidth / 2;
+    h = glass.offsetHeight / 2;
+    glass.addEventListener("mousemove", moveMagnifier);
+    img.addEventListener("mousemove", moveMagnifier);
+    glass.addEventListener("touchmove", moveMagnifier);
+    img.addEventListener("touchmove", moveMagnifier);
+    function moveMagnifier(e) {
+      var pos, x, y;
+      e.preventDefault();
+      pos = getCursorPos(e);
+      x = pos.x;
+      y = pos.y;
+      if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+      if (x < w / zoom) {x = w / zoom;}
+      if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+      if (y < h / zoom) {y = h / zoom;}
+      glass.style.left = (x - w) + "px";
+      glass.style.top = (y - h) + "px";
+      glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+    }
+    function getCursorPos(e) {
+      var a, x = 0, y = 0;
+      e = e || window.event;
+      a = img.getBoundingClientRect();
+      x = e.pageX - a.left;
+      y = e.pageY - a.top;
+      x = x - window.pageXOffset;
+      y = y - window.pageYOffset;
+      return {x : x, y : y};
+    }
+}
+magnify("myimage", 3);
+
+
+
+  //////  var slider = document.getElementById("myRange");
+  //////  var output = document.getElementById("demo");
+  //////  output.innerHTML = slider.value;
+    
+ //////   slider.oninput = function() {
+ //////     output.innerHTML = this.value;
+ //////   }
